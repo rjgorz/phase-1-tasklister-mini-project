c572c0a9267b9dcdd247d530b81a8c328e8c7607
Robert Gorz <rjgorz@gmail.com>
2023-01-11 16:40:05 -0600

    added functionality to create simple to do list. added features to edit/delete tasks, attach due dates, assign importance and change color of text based on the importance level

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      buildToDoList(e.target.new_task_description.value, e.target.new_task_due_date.value);
+      form.reset();
+  });
 });
+
+function buildToDoList(task, due) {
+  let li = document.createElement('li');
+  let delBtn = document.createElement('button');
+  const dropDown = document.querySelector('#new_task_importance');
+  delBtn.textContent = ' X ';
+  delBtn.contentEditable = false;
+  delBtn.addEventListener('click', e => e.target.parentNode.remove())
+  if(dropDown.value === 'high')
+    li.style.color = 'red';
+  else if(dropDown.value ==='medium')
+    li.style.color = 'orange';
+  else if(dropDown.value === 'low')
+    li.style.color = 'green';
+  li.textContent = `${task} - Due: ${due}  `;
+  li.contentEditable = true;
+  li.appendChild(delBtn);
+  document.querySelector('#tasks').appendChild(li)
+}
